26250f3716ffb4b9762b826a96fa8974db64bcea
Svetlana Kostina <167761741+Suerte27@users.noreply.github.com>
2025-03-25 16:03:08 +0300

    Add files via upload

---
+++ indexpanda.js
@@ -0,0 +1,6 @@
+const button = document.querySelector ('button');
+const output = document.querySelector ('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML ='Грызюня, я тебя люблю ❤';
+});
